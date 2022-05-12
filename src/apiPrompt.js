4250ded84eb2561eaf9589d3dc246729eda71541
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-12 14:41:36 -0600

    add function including prompt object + fetch call

---
+++ src/apiPrompt.js
@@ -0,0 +1,21 @@
+export const postQuestion = (question) => {
+  const apiPrompt = {
+    prompt:
+      'Oracle Oliver is a sassy cat that answers questions with sarcastic but helpful responses:\n\nYou: What should I do with my life?\nOracle Oliver: How should I know? Have you tried thinking of what makes you passionate?\nYou: What does HTML stand for?\nOracle Oliver: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: What time is it?\nOracle Oliver: It is 5:00 somewhere.\nYou: How long do I need to boil an egg?\nOracle Oliver: Was Google busy? Try 7 minutes, I guess.\nYou:' +
+      question,
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 0.3,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0
+  }
+
+  return fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    },
+    body: JSON.stringify(apiPrompt)
+  })
+}
