8253317f027f2fef2f639203b1e0438f414b6c95
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-11 22:05:31 -0600

    Remove default code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Oracle Oliver</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
