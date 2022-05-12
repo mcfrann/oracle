e4788415fc51b931e3c8c358f4cbd8d8c4d4559d
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-12 10:55:51 -0600

    Import Form, create state format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css'
 import { useState } from 'react'
+import Form from './Components/Form/Form.js'
 
 const App = () => {
-  const [questions, setQuestions] = useState([])
-  const [answers, setAnswers] = useState([])
+  // const [currentQuestion, setCurrentQuestions] = useState(['You: '])
+  // const [currentAnswer, setCurrentAnswer] = useState(['Oracle Oliver: '])
+  const [converstion, setConversation] = useState([
+    {
+      You: '',
+      'Oracle Oliver': ''
+    }
+  ])
 
   return (
     <div className='App'>
       <h1>Oracle Oliver</h1>
+      <Form />
     </div>
   )
 }
