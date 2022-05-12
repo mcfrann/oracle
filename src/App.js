99228ba8ddc282f905d911535a12e72f5ed9235c
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-12 14:39:36 -0600

    add update, submit methods. return questContainer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import './App.css'
 import { useState } from 'react'
-import Form from './Components/Form/Form.js'
+import Form from './Components/Form/Form'
+import QuestionContainer from './Components/QuestionContainer/QuestionContainer'
+import { postQuestion } from './apiPrompt'
 
 const App = () => {
-  // const [currentQuestion, setCurrentQuestions] = useState(['You: '])
-  // const [currentAnswer, setCurrentAnswer] = useState(['Oracle Oliver: '])
-  const [converstion, setConversation] = useState([
+  const [conversation, setConversation] = useState([
     {
-      You: '',
-      'Oracle Oliver': ''
+      id: '',
+      you: '',
+      oracleOliver: ''
     }
   ])
+  const questionPrompt = conversation[0].you
+
+  const updateQuestion = (e) => {
+    setConversation([
+      {
+        id: '',
+        you: e.target.value,
+        oracleOliver: ''
+      }
+    ])
+  }
+
+  const submitQuestion = (e) => {
+    e.prevent.default(e)
+    postQuestion(questionPrompt)
+      .then((response) => response.json())
+      .then((response) =>
+        setConversation([
+          {
+            id: Date.now(),
+            you: questionPrompt,
+            oracleOliver: response
+          },
+          ...conversation
+        ])
+      )
+  }
 
   return (
     <div className='App'>
       <h1>Oracle Oliver</h1>
-      <Form />
+      <Form
+        updateQuestion={updateQuestion}
+        submitQuestion={submitQuestion}
+        value={questionPrompt}
+      />
+      <QuestionContainer conversation={conversation} />
     </div>
   )
 }
