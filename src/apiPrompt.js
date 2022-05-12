1462cca20412268e739f4a88f7fa27fd20cc9840
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-12 16:14:52 -0600

    change parameter name

--- src/apiPrompt.js
+++ src/apiPrompt.js
@@ -1,8 +1,8 @@
-export const postQuestion = (question) => {
+export const postQuestion = (questionPrompt) => {
   const apiPrompt = {
     prompt:
       'Oracle Oliver is a sassy cat that answers questions with sarcastic but helpful responses:\n\nYou: What should I do with my life?\nOracle Oliver: How should I know? Have you tried thinking of what makes you passionate?\nYou: What does HTML stand for?\nOracle Oliver: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: What time is it?\nOracle Oliver: It is 5:00 somewhere.\nYou: How long do I need to boil an egg?\nOracle Oliver: Was Google busy? Try 7 minutes, I guess.\nYou:' +
-      question,
+      questionPrompt,
     temperature: 0.5,
     max_tokens: 60,
     top_p: 0.3,
