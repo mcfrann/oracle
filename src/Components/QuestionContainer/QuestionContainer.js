ff7e1fd0c50e80b98ae7a574ab5a96b73d137ae8
Francesca McConnell <fmcconnell59@gmail.com>
2022-05-12 21:04:09 -0600

    Uncomment if else statement for QuestionTile

--- src/Components/QuestionContainer/QuestionContainer.js
+++ src/Components/QuestionContainer/QuestionContainer.js
@@ -2,19 +2,18 @@ import QuestionTile from '../QuestionTile/QuestionTile'
 
 const QuestionContainer = ({ conversation }) => {
   const questions = conversation.map((convo) => {
-    // if (convo.you && convo.oracleOliver) {
-    return (
-      <QuestionTile
-        id={convo.id}
-        key={convo.id}
-        question={convo.you}
-        answer={convo.oracleOliver}
-      />
-    )
-    // }
-    //  else {
-    //   return <h1 key='none'>Ask Oracle Oliver a question...</h1>
-    // }
+    if (convo.you && convo.oracleOliver) {
+      return (
+        <QuestionTile
+          id={convo.id}
+          key={convo.id}
+          question={convo.you}
+          answer={convo.oracleOliver}
+        />
+      )
+    } else {
+      return <h1 key='none'>Ask Oracle Oliver a question...</h1>
+    }
   })
 
   return <section className='questionContainer'>{questions}</section>
